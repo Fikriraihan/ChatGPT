cc9ab134e1ea3876cd0f6298d9409656663e9ba4
Fikri Raihan <fikri.raihan4@gmail.com>
2023-05-02 17:49:27 +0700

    add model

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -6,6 +6,8 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
+import ModelSelection from "./ModelSelection";
+import useSWR from "swr";
 
 type Props = {
   chatId: string;
@@ -15,8 +17,9 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
-  //   useSWR to get model
-  const model = "text-davinci-003";
+  const { data: model } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
 
   const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -76,7 +79,9 @@ function ChatInput({ chatId }: Props) {
         </button>
       </form>
 
-      <div>{/* ModelSelection */}</div>
+      <div className="md:hidden">
+        <ModelSelection />
+      </div>
     </div>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase";
 import ChatRow from "./ChatRow";
+import ModelSelection from "./ModelSelection";
 
 function Sidebar() {
   const { data: session } = useSession();
@@ -18,12 +19,21 @@ function Sidebar() {
         <div>
           <NewChat />
 
-          <div>{/* ModelSelection */} </div>
+          <div className="hidden sm:inline">
+            <ModelSelection />
+          </div>
 
           {/* Map through the ChatRows */}
-          {chats?.docs.map((chat) => (
-            <ChatRow key={chat.id} id={chat.id} />
-          ))}
+          <div className="flex flex-col space-y-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading chats...</p>
+              </div>
+            )}
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
       </div>
 
